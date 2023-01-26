0eaf1014b08dbf849ba40faba6d2e641fe4d7dc6
Juzaweb <dangtheanh16@gmail.com>
2023-01-26 13:54:06 +0700

    :construction: Server-Side Rendering

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
-const path = require( 'path' );
+import path from 'path';
+import fs from 'fs';
 
-// ignore `.scss` imports
-require( 'ignore-styles' );
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import express from 'express';
 
-// transpile imports on the fly
-require( '@babel/register')( {
-    configFile: path.resolve( __dirname, '../babel.config.js' ),
-});
+import App from '../src/App';
 
-// import express server
-require( './express.js' );
+const PORT = process.env.PORT || 3006;
+const app = express();
+
+app.get('/', (req, res) => {
+    const app = ReactDOMServer.renderToString(<App />);
+    const indexFile = path.resolve('./build/index.html');
+  
+    fs.readFile(indexFile, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Something went wrong:', err);
+        return res.status(500).send('Oops, better luck next time!');
+      }
+  
+      return res.send(
+        data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+      );
+    });
+  });
+  
+  app.use(express.static('./build'));
+  
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+  
